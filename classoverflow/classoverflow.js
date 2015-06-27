a4459d172b400a917b2e3b332b7236747ad12b2a
eglassman <elena.glassman@gmail.com>
2015-06-27 00:21:57 -0700

    added class page and a page for 6004

--- classoverflow/classoverflow.js
+++ classoverflow/classoverflow.js
@@ -2,6 +2,12 @@ if (Meteor.isClient) {
     Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+    Template.classes.helpers({
+    classes: [
+      { text: "6.004" , route: 'class6004'},
+      { text: "Class 2" }
+    ]
+  });
 }
 
 if (Meteor.isServer) {
@@ -9,3 +15,14 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Router.map(function () {
+  this.route('about');  // By default, path = '/about', template = 'about'
+  this.route('classes', {
+    path: '/',  //overrides the default '/home'
+    //data: function () {return Classes.find()}
+  });
+  this.route('class6004', {
+    data: function () {return true} //{return Articles.find({class: '6.004'})}  //set template data context
+  });
+});
