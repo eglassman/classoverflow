9268a418a8cff7ad9af30f16ddbd957838dc7c30
eglassman <elena.glassman@gmail.com>
2015-06-27 18:01:52 -0700

    a little more dry-friendly

--- classoverflow/classoverflow.js
+++ classoverflow/classoverflow.js
@@ -1,11 +1,17 @@
 if (Meteor.isClient) {
     Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
+        passwordSignupFields: "USERNAME_ONLY"
+    });
     Template.classes.helpers({
     classes: [
       { text: "6.004" , route: '6004class'},
       { text: "6.005" , route: '6005class'}
+    ]
+  });
+    Template.navbar6004.helpers({
+    errorCoords: [
+      { name: "module" , placeholder: 'Module'},
+      { name: "testNum" , placeholder: 'Test Number'}
     ]
   });
 }
@@ -27,5 +33,10 @@ Router.map(function () {
   });
   this.route('6005class', {
     data: function () {return true} //{return Articles.find({class: '6.004'})}  //set template data context
+  });
+  this.route('class', { //I'm not using this route yet, because the composition of a class page is still by hand right now. There is no generic class template--yet!
+    path: '/class/:classNum',
+    data: function () {return true},
+    template: 'class',
   });
 });
